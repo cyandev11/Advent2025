1bc57c41f4b8af41b22664965b41731b25bf3dec
Ryan Liu <ryanliu12620@gmail.com>
2025-12-02 00:53:33 -0500

    part1 done, simple solution that checks all numbers within the range, then if the halves match

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,5 +1,26 @@
+import re
+
 def part1(moves):
-    print()
+    total_invalid_ids = 0
+    for move in moves:
+        ranges = move.split("-")
+        lower_range = ranges[0]
+        higher_range = ranges[1]
+        total_invalid_ids += check_invalid(int(lower_range), int(higher_range))
+    return total_invalid_ids
+        
+def check_invalid(lower, higher):
+    total_invalid = 0
+    while lower <= higher:
+        is_invalid = check_halfs(lower)
+        if is_invalid:
+            total_invalid += lower
+        lower += 1
+    return total_invalid
+
+def check_halfs(id):
+    half_len = len(str(id)) // 2
+    return str(id)[:half_len] == str(id)[half_len:]
 
 
 # Opens a file and splits lines into inputs
@@ -7,9 +28,10 @@ def open_file(filename):
     # opens file and returns all strings
     with open(filename, "r") as file_input:
         input = file_input.read()
-        list_of_values = input.split("\n")
+        list_of_values = input.split(",")
         return list_of_values
 
 
 inputs = open_file("Day2/Day2Input.txt")
+print(inputs)
 print(part1(inputs))
