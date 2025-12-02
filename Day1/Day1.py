4e1ac195326cfc6125b9912ed62d8813193fb700
Ryan Liu <ryanliu12620@gmail.com>
2025-12-01 22:35:41 -0500

    part1 features a solution with a dial that jumps instananeously, only featuring the ending value part2 features a one by one solution in which the dial is turned one by one and noted down for all zeros

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -5,8 +5,8 @@ def part1(moves):
     current_dial = 50
     times_at_zero = 0
     for move in moves:
-        num_of_pass = move_lock(current_dial, move)
-        if num_of_pass > 0:
+        current_dial = move_lock(current_dial, move)
+        if current_dial == 0:
             times_at_zero += 1
     return times_at_zero
 
@@ -14,30 +14,48 @@ def part2(moves):
     current_dial = 50
     times_at_zero = 0
     for move in moves:
-        current_dial = move_lock(current_dial, move)
-        if current_dial == 0:
-            times_at_zero += 1
+        direction = move[0]
+        steps = int(move[1:])
+        for i in range(steps):
+            # move dial one by one
+            if direction == "R":
+                current_dial += 1
+            if direction == "L":
+                current_dial -= 1
+            # adjust dials
+            if current_dial == 100:
+                current_dial = 0
+            if current_dial < 0:
+                current_dial = 100 - abs(current_dial)
+            # if zero, add
+            if current_dial == 0:
+                times_at_zero += 1
     return times_at_zero
 
+
+
+
+# moves lock according to direction
 def move_lock(current_dial, move):
+    # find direction and how many steps
     direction = move[0]
     steps = int(move[1:])
+
     if direction == "R":
         current_dial += steps
     if direction == "L":
         current_dial -= steps
 
-    number_of_passes = abs(current_dial // 100)
     current_dial = current_dial % 100
 
     if current_dial == 100:
         current_dial = 0
     if current_dial < 0:
         current_dial = 100 - abs(current_dial)
-    if current_dial == 0:
-        number_of_passes += 1
 
-    return number_of_passes
+    return current_dial
+
+
 
 def open_file(filename):
     # opens file and returns all strings
@@ -47,5 +65,6 @@ def open_file(filename):
         return list_of_values
 
 
-inputs = open_file("Day1Input")
+inputs = open_file("Day1/Day1Input.txt")
 print(part1(inputs))
+print(part2(inputs))
