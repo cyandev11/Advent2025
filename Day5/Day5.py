6998b3ef0a8a4d2c03bbeb8f6b2eb4e9dc5621e0
Ryan Liu <ryanliu12620@gmail.com>
2025-12-05 13:01:28 -0500

    looks like i need to change sorting with custom sort

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,5 +1,6 @@
 import textwrap
-import re
+# pip install natsort
+from natsort import natsorted
 
 
 def part1(moves):
@@ -15,13 +16,54 @@ def part1(moves):
             lower = range[0]
             higher = range[1]
             # if the ingredient is bt in any range
-            if int(lower) <= int(ingredient) <= int(higher) and found == False:
+            if int(lower) <= int(ingredient) <= int (higher) and found == False:
                 # found gets set to true so there are no repeats and we count it as fresh
                 found = True
                 num_fresh += 1
 
     return num_fresh
 
+def part2(moves):
+    id_ranges = moves[0]
+    sorted_id_ranges = natsorted(id_ranges)
+    index = 0
+    total = 0
+    # now the ranges are naturally sorted according to lower end values, i can find
+    while index < len(sorted_id_ranges) - 1:
+        current_range = sorted_id_ranges[index]
+        next_range = sorted_id_ranges[index + 1]
+        index += 1
+        can_merge = check_merge(current_range, next_range)
+        if can_merge:
+            new_merge = merge(current_range, next_range)
+            sorted_id_ranges.insert(index, new_merge)
+            sorted_id_ranges.remove(current_range)
+            sorted_id_ranges.remove(next_range)
+            sorted_id_ranges = natsorted(sorted_id_ranges)
+            index = 0
+    print(sorted_id_ranges)
+    for id_range in sorted_id_ranges:
+        total += check_diff(id_range)
+    return total
+    
+def check_merge(range1, range2):
+    # 2nd and 3rd num
+    higher1 = range1.split("-")[1]
+    lower2 = range2.split("-")[0]
+    if int(higher1) >= int(lower2):
+        return True
+
+def merge(range1, range2):
+    # 1st and 4th num
+    lower1 = range1.split("-")[0]
+    higher2 = range2.split("-")[1]
+    return lower1 + "-" + higher2
+
+def check_diff(range):
+    lower = range.split("-")[0]
+    higher = range.split("-")[1]
+    diff = int(higher) - int(lower) + 1
+    return diff
 
 # Opens a file and splits lines into inputs
 def open_file(filename):
@@ -37,3 +79,4 @@ def open_file(filename):
 # inputs is a tuple with ingredient ids and ingredients to check
 inputs = open_file("Day5/Day5Input.txt")
 print(part1(inputs))
+print(part2(inputs))
