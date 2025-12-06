4884348d7462cb24500254e0ed933876c3d7075f
Ryan Liu <ryanliu12620@gmail.com>
2025-12-06 15:06:05 -0500

    so part 1 had me spliting, getting rid of whitespace, and transposeing.
    for part2 i tried just readining it from the opposite when i realized that blank spaces can be both left and right.
    instead i pivoted to a indexing strat where i iterated through the input using the bottom row with the operations.

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,23 +1,65 @@
-def part1(problems, operations):
-    # solve for equation
-    columns = len(problems[0])
-    rows = len(problems)
+import numpy
 
+
+def part1(moves):
+    # split all into lists and remove white spaces
+    for i, problems in enumerate(moves):
+        problems = problems.split(" ")
+        while "" in problems:
+            problems.remove("")
+        moves[i] = problems
+    # transpose array
+    moves = numpy.transpose(moves).tolist()
     total = 0
-    solution = 0
-    for col in range(columns):
-        operation = operations[col]
-        if operation == "*":
-            solution = 1
-        else:
-            solution = 0
-        for row in range(rows):
-            if operation == "*":
-                solution *= int(list_problems[row][col])
-            else:
-                solution += int(list_problems[row][col])
+    # for every equation, solve the equation depending on the operation
+    for equation in moves:
+        operation = equation[-1]
+        numbers = equation[:-1]
+        total += solve_equation(numbers, operation)
+    return total
+
+
+def part2(moves):
+    numbers = moves[:-1]
+    operations = moves[-1]
+    total = 0
+    # find ranges for each eqaution with the last row
+    indexs = []
+    for i, operation in enumerate(operations):
+        if operation == "*" or operation == "+":
+            indexs.append(i)
+    indexs.append(len(moves[0]))
+
+    for i, index in enumerate(indexs[:-1]):
+        lower_index = indexs[i]
+        higher_index = indexs[i + 1]
+
+        equation = ""
+        while higher_index > lower_index:
+            higher_index -= 1
+            for number in numbers:
+                equation += number[higher_index]
+            equation += " "
+
+        current_eqaution = equation.split(" ")
+        while "" in current_eqaution:
+            current_eqaution.remove("")
+        current_operation = operations[lower_index]
+        solution = solve_equation(current_eqaution, current_operation)
         total += solution
     return total
+
+
+def solve_equation(numbers, operation):
+    solution = 0
+    if operation == "*":
+        solution = 1
+    for number in numbers:
+        if operation == "*":
+            solution *= int(number)
+        else:
+            solution += int(number)
+    return solution
 
 
 # Opens a file and splits lines into inputs
@@ -31,21 +73,8 @@ def open_file(filename):
         return list_of_values
 
 
-# inputs is a tuple with ingredient ids and ingredients to check
 inputs = open_file("Day6/Day6Input.txt")
-# i get it, there are 1000 operations and 4000 words (numbers)
-# find the number of and the type of operation first
-length_inputs = len(inputs) - 1
-list_problems = inputs[:length_inputs]
-operations = inputs[length_inputs].split(" ")
+print(part1(inputs))
 
-# split all into lists and remove white spaces
-while "" in operations:
-    operations.remove("")
-for i, problems in enumerate(list_problems):
-    problems = problems.split(" ")
-    while "" in problems:
-        problems.remove("")
-    list_problems[i] = problems
-
-print(part1(list_problems, operations))
+inputs = open_file("Day6/Day6Input.txt")
+print(part2(inputs))
