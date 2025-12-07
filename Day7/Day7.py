0d54d51d3e30de161fd9dac3575bbd3f914475ad
Ryan Liu <ryanliu12620@gmail.com>
2025-12-07 16:44:18 -0500

    Just had an epiphany moment

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -1,24 +1,43 @@
 import numpy
 import textwrap
 
+
 def part1(map):
-    current_row = 0
+    total_splits = 0
+    # sends beams down
+    for r, row in enumerate(map[:-1]):
+        for c, col in enumerate(row):
+            current_symbol = map[r][c]
+            if current_symbol == "S":
+                map[r + 1][c] = "|"
+            elif current_symbol == "|" and map[r + 1][c] == "^":
+                map[r + 1][c - 1] = "|"
+                map[r + 1][c + 1] = "|"
+                total_splits += 1
+            elif current_symbol == "|" and map[r + 1][c] == ".":
+                map[r + 1][c] = "|"
+        # print_2d_array(map) # for animation
+    return total_splits
+
+
+def part2(map):
     total_beams = 0
     # sends beams down
     for r, row in enumerate(map[:-1]):
         for c, col in enumerate(row):
             current_symbol = map[r][c]
             if current_symbol == "S":
-                map[r+1][c] = "|"
-            elif current_symbol == "|" and map[r+1][c] == "^":
-                map[r+1][c-1] = "|"
-                map[r+1][c+1] = "|"
-                total_beams += 1
-            elif current_symbol == "|" and map[r+1][c] == ".":
-                map[r+1][c] = "|"
-            current_row += 1
-        print_2d_array(map)
-    return total_beams
+                map[r + 1][c] = "|"
+            elif current_symbol == "|" and map[r + 1][c] == "^":
+                map[r + 1][c - 1] = "|"
+                map[r + 1][c + 1] = "|"
+                total_beams += 0
+            elif current_symbol == "|" and map[r + 1][c] == ".":
+                map[r + 1][c] = "|"
+        # print_2d_array(map)  # for animation
+
+    return 0
+
 
 # Opens a file and splits lines into inputs
 def open_file(filename):
@@ -31,12 +50,14 @@ def open_file(filename):
             map.append(textwrap.wrap(row, 1))
         return map
 
+
 def print_2d_array(array):
     for row in array:
         for col in row:
-            print(col, end='') 
+            print(col, end="")
         print()
 
 
 inputs = open_file("Day7/Day7Input.txt")
 print(part1(inputs))
+print(part2(inputs))
